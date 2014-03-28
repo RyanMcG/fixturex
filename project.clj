58746a118208dc4683be903bedae1ff5cc73f296
Ryan McGowan <ryan@ryanmcg.com>
2014-03-28 08:53:35 -0700

    Change name to fixturex.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject var-fixtures "0.1.0-SNAPSHOT"
+(defproject fixturex "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

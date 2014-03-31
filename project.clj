b4363a4db2febdd7fc157409288d72d2bf897f42
Ryan McGowan <ryan@ryanmcg.com>
2014-03-30 17:57:02 -0700

    Define codox config.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[com.ryanmcg/incise-codox "0.1.0"]]
+                   :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}}})

eb9f34e5301e2c0df23d2ab4249321686498b34e
Ryan McGowan <ryan@ryanmcg.com>
2014-04-03 18:26:58 -0700

    Make README root of website.

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[com.ryanmcg/incise-codox "0.1.0"]]
+  :profiles {:dev {:dependencies [[incise-markdown-parser "0.1.0"]
+                                  [com.ryanmcg/incise-codox "0.1.0"]
+                                  [incise-core "0.3.2"]]
                    :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}}})

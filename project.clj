5f3a4f2efc599cdac08632587ade165e35567973
Ryan McGowan <ryan@ryanmcg.com>
2014-05-16 20:49:28 -0700

    Add git deployer to project.clj.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[incise-markdown-parser "0.1.0"]
+                                  [incise-git-deployer "0.1.0"]
                                   [com.ryanmcg/incise-codox "0.1.0"]
                                   [incise-core "0.3.2"]
                                   [com.ryanmcg/incise-vm-layout "0.4.0"]]

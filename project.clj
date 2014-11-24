cc1907526b73f6c9c3756a3e0852b21047977081
Ryan McGowan <ryan@ryanmcg.com>
2014-11-23 23:25:12 -0800

    Upgrade various deps.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[incise-markdown-parser "0.1.0"]
+  :profiles {:dev {:dependencies [[incise-markdown-parser "0.2.0"]
                                   [incise-git-deployer "0.1.0"]
                                   [com.ryanmcg/incise-codox "0.1.0"]
-                                  [incise-core "0.3.2"]
-                                  [com.ryanmcg/incise-vm-layout "0.4.0"]]
+                                  [incise-core "0.4.0"]
+                                  [com.ryanmcg/incise-vm-layout "0.5.0"]]
                    :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}}})

bf29daa811787f053f7eb258873612be758d2fc1
Ryan McGowan <ryan@ryanmcg.com>
2014-11-23 23:24:28 -0800

    Set url correctly.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fixturex "0.1.0"
   :description "A library of helpful test fixture macros and functions."
-  :url "http://example.com/FIXME"
+  :url "http://www.ryanmcg.com/fixturex/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]

ae2822d00df1cfd1ea13c17e015a48bd7b743b7a
Ryan McGowan <ryan@ryanmcg.com>
2015-02-04 20:21:06 -0800

    Upgrade version of incise being used.

--- project.clj
+++ project.clj
@@ -7,10 +7,8 @@
   :repack [{:type :clojure
             :levels 1
             :path "src"}]
-  :profiles {:dev {:dependencies [[incise-markdown-parser "0.2.0"]
-                                  [incise-git-deployer "0.1.0"]
+  :profiles {:dev {:dependencies [[incise "0.5.0"]
                                   [com.ryanmcg/incise-codox "0.1.0"]
-                                  [incise-core "0.4.0"]
                                   [com.ryanmcg/incise-vm-layout "0.5.0"]]
                    :plugins [[lein-repack "0.2.4"]]
                    :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}}
